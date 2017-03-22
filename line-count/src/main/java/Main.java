d43764a96797201b26778b71fead681638803249
anhvurz90 <anhvurz90@gmail.com>
2017-03-22 23:18:58 +0700

    Add first Spark example: line count

---
+++ line-count/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * 
+ * Run Spark:
+ * 
+ * anhvu@Lenovo-Y70:~/01.Work/15.Spark/spark-2.1.0-bin-hadoop2.7/bin$ ./spark-submit --class Main 
+ *                      --master local[2] 
+ *        ~/01.Work/03.Projects/git/tutorials/spark/line-count/target/line-count-0.0.1-SNAPSHOT.jar 
+ * 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("Line Count");
+        JavaSparkContext ctx = new JavaSparkContext(conf);
+        
+        JavaRDD<String> textLoad = ctx.textFile("/home/anhvu/01.Work/15.Spark/spark-2.1.0-bin-hadoop2.7/README.md");
+        
+        System.out.println(textLoad.count());
+        
+    }
+}
