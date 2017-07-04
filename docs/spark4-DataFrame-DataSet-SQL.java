707812eccd945dc30b21e5a561c4f522f006eb6d
anhvurz90 <anhvurz90@gmail.com>
2017-07-04 16:44:48 +0700

    Update spark-4-DataFrame-DataSet-SQL.java

--- docs/spark4-DataFrame-DataSet-SQL.java
+++ docs/spark4-DataFrame-DataSet-SQL.java
@@ -16,7 +16,7 @@
 		4.2.1.Starting Point: SparkSession {
 			import org.apache.spark.sql.SparkSession;
 			
-			SparkSession session = SparkSession
+			SparkSession spark = SparkSession
 				.builder()
 				.appName("Java Spark SQL basic example")
 				.config("spark.some.config.option", "some-value")
@@ -26,7 +26,9 @@
 			import org.apache.spark.sql.Dataset;
 			import org.apache.spark.sql.Row;
 			
-			Dataset<Row> df = session.read().json("examples/src/main/resources/people.json");
+			Dataset<Row> df = spark.read().json("examples/src/main/resources/people.json");
+			
+			// Displays the content of the DataFrame to stdout
 			df.show();
 			//age | name	|
 			//30	 | Andy	|
@@ -34,10 +36,19 @@
 		4.2.3.Untyped Dataset Operations (aka DataFrame Opertaions) {
 			import static org.apache.spark.sql.functions.col;
 			
+			// Print the schema in a tree format
 			df.printSchema();
+			
+			// Select only the "name" column
 			df.select("name").show();
+			
+			// Select everybody, but increment the 'age' by 1
 			df.select(col("name"), col("age").plus(1)).show();
+			
+			// Select people older than 21
 			df.filter(col("age").gt(21)).show();
+			
+			// Count people by age
 			df.groupBy("age").count().show();
 			
 			API: http://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/Dataset.html
@@ -47,13 +58,62 @@
 			import org.apache.spark.sql.Dataset;
 			import org.apache.spark.sql.Row;
 			
+			// Register the DataFrame as a SQL temporary view
 			df.createOrReplaceTempView("people");
+			
 			Dataset<Row> sqlDF = spark.sql("SELECT * FROM people");
 			sqlDF.show();
 		}
 		4.2.5.Global Temporary View {
+			// Register the DataFrame as a global temporary view
+			df.createGlobalTempView("people");
+			
+			// Global temporary view is tied to a system preserved database 'global_temp'
+			spark.sql("SELECT * FROM global_temp.people").show();
+			
+			// Global temporary view is cross-session
+			spark.newSession().sql("SELECT * FROM global_temp.people").show();
 		}
-		Creating Datasets
+		4.2.6.Creating Datasets {
+			import org.apache.spark.api.java.function.MapFunction;
+			import org.apache.spark.sql.Dataset;
+			import org.apacke.spark.sql.Row;
+			import org.apache.spark.sql.Encoder;
+			import org.apache.spark.sql.Encoders;
+			
+			public static class Person implements Serializable {
+				String name; int age;
+			}
+			
+			Person person = new Person("Andy", 32);
+			
+			// Encoders are created for Java beans
+			Encoder<Person> personEncoder = Encoders.bean(Person.class);
+			Dataset<Person> javaBeanDS = spark.createDataset(
+				Collections.singletonList(person),
+				personEncoder
+			);
+			
+			javaBeanDS.show();
+
+			// Encoders for most common types are provided in class Encoders
+			Encoder<Integer> integerEncoder = Encoders.INT();
+			Dataset<Integer> primitiveDS = spark.createDataset(Arrays.asList(1, 2, 3), integerEncoder);
+			
+			Dataset<Integer> transformedDS = primitiveDS.map(
+				new MapFunction<Integer, Integer>() {
+					@Override
+					public Integer call(Integer value) throws Exception {
+						return value + 1;
+					}
+				}, integerEncoder);				
+			transformedDS.collect(); // Returns [2, 3, 4]
+				
+			// DataFrames can be converted to a Dataset by providing a class. Mapping based on name
+			String path = "examples/src/main/resources/people.json";
+			Dataset<Person> peopleDS = spark.read().json(path).as(personEncoder);
+			peopleDS.show();
+		}
 		Interoperating with RDDs
 			Inferring the Schema Using Reflection
 			Programmatically Specifying the Schema
