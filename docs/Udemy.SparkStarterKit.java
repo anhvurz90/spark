b5a651a435fff09b63f7fe19d4f4bf305b09f698
anhvurz90 <anhvurz90@gmail.com>
2017-07-11 11:21:21 +0700

    Add Udemy.SparkStarterKit.01-Let'sGetStarted

---
+++ docs/Udemy.SparkStarterKit.java
@@ -0,0 +1,31 @@
+UDEMY - SPARK STARTER KIT
+1.Let's Get Started: {
+  1.1.Prerequisites {
+    - Hadoop Starter Kit
+    - A Short Chapter on Scala
+  }
+  1.2.Say Hi to Spark: {
+    - Spark vs Hadoop - Who Wins?
+    - Challenges Spark tries to address
+    - How Spark is faster than Hadoop?
+  }
+  1.3.Introduction to RDD: {
+    - The need for RDD
+    - What is RDD?
+    - What an RDD is not?
+  }
+  1.4.Spark's execution behind the scenes: {
+    - First program in Spark
+    - What are denendencies & why they are important?
+    - Program to Execution (Part1 & 2)
+  }
+  1.5.Fault tolerance in In-memory computing: {
+    - Fault Tolerance
+    - Memory Management
+  }
+  1.6.A Short chapter on Scala: {
+    - Introduction to Scale
+    - First program in Scala (not hello world)
+    - Scala functions
+  }
+}
